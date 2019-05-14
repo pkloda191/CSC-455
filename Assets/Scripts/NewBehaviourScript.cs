c4e3fb4752bed199e6b968bcf32471d0e94b9853
Peter <pkloda191@gmail.com>
2019-05-13 23:03:04 -0500

    initial setup

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NewBehaviourScript : NetworkBehaviour
+{
+	[SerializeField]
+	private float distance = 1.0f;
+	private int range = 2;
+	private RaycastHit hit;
+	[SyncVar]
+	private GameObject objectID;
+	private NetworkIdentity objNetId;
+
+	private int layerMask = 1 << 9; //pickUpLayer is 9
+
+	void Update()
+	{
+		if (isLocalPlayer)
+		{
+			CheckIfMoving();
+		}
+	}
+
+	void CheckIfMoving()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		Debug.DrawRay(ray.origin, ray.direction * 100, Color.cyan);
+
+
+		if (isLocalPlayer && Input.GetMouseButton(0))
+		{
+
+			if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, layerMask))
+			{
+				objectID = GameObject.Find(hit.transform.name);                                    // this gets the object that is hit
+				CmdPickUp(objectID);
+			}
+		}
+	}
+
+
+	[ClientRpc]
+	void RpcPickUp(GameObject obj, Vector3 objPosition, Quaternion objRotation)
+	{
+		obj.transform.position = objPosition;
+		obj.transform.rotation = objRotation;
+	}
+
+	[Command]
+	void CmdPickUp(GameObject obj)
+	{
+		objNetId = obj.GetComponent<NetworkIdentity>();        // get the object's network ID
+		objNetId.AssignClientAuthority(connectionToClient);    // assign authority to the player who is changing the color
+
+		Camera playerCamera = Camera.main;
+		Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+		Vector3 objPosition = playerCamera.ScreenToWorldPoint(mousePosition);
+		Quaternion objRotation = playerCamera.transform.rotation;
+
+		RpcPickUp(obj, objPosition, objRotation);                                    // use a Client RPC function to "paint" the object on all clients
+
+		this.GetComponent<Renderer>().material.color = Color.clear; //finally change color to transparent
+		objNetId.RemoveClientAuthority(connectionToClient);    // remove the authority from the player who changed the color
+	}
+}
---
+++ Assets/Scripts/ThrowObject.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowObject : MonoBehaviour
+{
+	public float throwForce = 200;
+	bool hasPlayer = false;
+	bool beingCarried = false;
+	private bool touched = false;
+	public float powerMult = 0;
+
+	void Update()
+	{
+		GameObject player2 = GameObject.Find("FPS Player(Clone)");
+		float dist = Vector3.Distance(gameObject.transform.position, player2.transform.position);
+		//Debug.Log ("" + dist);
+		if (dist <= 1.5f)
+		{
+			hasPlayer = true;
+		}
+		else
+		{
+			hasPlayer = false;
+		}
+		if (hasPlayer)
+			//&& Input.GetKeyDown(KeyCode.E)
+		{
+			GetComponent<Rigidbody>().isKinematic = true;
+			transform.position = new Vector3 (transform.position.x, transform.position.y + 1, transform.position.z);
+			transform.rotation = Quaternion.Euler (0,0,90);
+			transform.parent = player2.transform;
+			beingCarried = true;
+			powerMult = 0;
+		}
+		if (beingCarried)
+		{
+			if (touched)
+			{
+				GetComponent<Rigidbody>().isKinematic = false;
+				transform.parent = null;
+				beingCarried = false;
+				touched = false;
+			}
+			if (Input.GetMouseButton(0) && powerMult <= 10)
+			{
+				powerMult += 10 * Time.deltaTime; // Cap at some max value too
+			}
+			else if (Input.GetMouseButtonDown(1))
+			{
+				GetComponent<Rigidbody>().isKinematic = false;
+				transform.parent = null;
+				beingCarried = false;
+			}
+			if (Input.GetMouseButtonUp (0)) {
+				GetComponent<Rigidbody>().isKinematic = false;
+				transform.parent = null;
+				beingCarried = false;
+				GameObject camera = GameObject.FindGameObjectWithTag ("MainCamera");
+				GetComponent<Rigidbody> ().AddForce (camera.transform.forward * throwForce * powerMult, ForceMode.Acceleration);
+				GetComponent<Rigidbody> ().AddForce (camera.transform.up * throwForce * powerMult/2);
+			}
+		}
+	}
+
+	void OnTriggerEnter()
+	{
+		if (beingCarried)
+		{
+			touched = true;
+		}
+	}
+}
